d5724282078375c9cd59557ffd56d79aacca5203
Sokolov Yura <funny.falcon@gmail.com>
2011-11-21 15:21:11 +0400

    fix indention

--- split_pgdump.rb
+++ split_pgdump.rb
@@ -24,32 +24,32 @@ table, columns = nil, nil
 table_file_name, table_file = nil, nil
 File.open(output_file, 'w') do |out|
   STDIN.each_line do |line|
-      case state
-      when :schema
-        if line =~ /^COPY (\w+) \(([^)]+)\) FROM stdin;/
-          table, columns = $1, $2.split(', ')
-          table_file_name = File.join(tables_dir, table)
-          table_file = File.new(table_file_name, 'w')
-          puts "Start to write table #{table}" if $debug
-          state = :table
-        else
-          out.write line
-        end
-      when :table
-        if line =~ /^\\\.[\r\n]/
-          table_file.close
-          puts "Sorting table #{table}" if $debug
-          system(sort, '-n', '-o', table_file_name, table_file_name)
-          File.open(table_file_name, 'a'){|f| f.write(line)}
-          out.write "\\copy #{table} (#{columns.join(', ')}) from #{table_file_name}"
-          state = :schema
-          table, columns = nil, nil
-          table_file_name = nil
-          table_file = nil
-        else
-          table_file.write(line)
-        end
+    case state
+    when :schema
+      if line =~ /^COPY (\w+) \(([^)]+)\) FROM stdin;/
+        table, columns = $1, $2.split(', ')
+        table_file_name = File.join(tables_dir, table)
+        table_file = File.new(table_file_name, 'w')
+        puts "Start to write table #{table}" if $debug
+        state = :table
+      else
+        out.write line
       end
+    when :table
+      if line =~ /^\\\.[\r\n]/
+        table_file.close
+        puts "Sorting table #{table}" if $debug
+        system(sort, '-n', '-o', table_file_name, table_file_name)
+        File.open(table_file_name, 'a'){|f| f.write(line)}
+        out.write "\\copy #{table} (#{columns.join(', ')}) from #{table_file_name}"
+        state = :schema
+        table, columns = nil, nil
+        table_file_name = nil
+        table_file = nil
+      else
+        table_file.write(line)
+      end
+    end
   end
 end
 
